fa80cab1f75459558741bf3e689187046a3adac7
Mudkip909 <76056631+Mudkip909@users.noreply.github.com>
2021-09-26 13:25:06 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace ClassLibarary1    
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class BrrtMod : MonoBehaviour
+    {
+        public void Update()
+        {
+            double vesselSpeed = FlightGlobals.ActiveVessel.srfSpeed;
+            if (vesselSpeed > 0)
+            {
+                vesselSpeed /= 10;
+                var vessel = FlightGlobals.ActiveVessel;
+                var dvThing = vessel.transform.up;
+                dvThing *= (float)vesselSpeed;
+                vessel.ChangeWorldVelocity(dvThing);
+            }
+        }
+    }
+}
